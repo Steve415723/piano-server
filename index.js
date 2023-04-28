b4ae3fca4d5ad932ca70a1ff4d5d7e0333b03167
unknown <tyuplus5@gmail.com>
2023-04-28 15:31:17 +0900

    5000

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ console.log(`웹소켓 서버 실행 완료!`)
 let clients = {}
 let idlist = {}
 
-const server = new Server({port:9090})
+const server = new Server({port:5000})
 
 function broadcastall(message) {
     server.clients.forEach(client => {
