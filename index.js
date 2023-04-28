4054a0912bd1181334b42dbbf1b6ab7072529643
unknown <tyuplus5@gmail.com>
2023-04-28 14:45:11 +0900

    웹소켓 모듈 변경

--- index.js
+++ index.js
@@ -1,58 +1,72 @@
-const app = require("express")()
-const http = require('http').createServer(app);
-const io = require("socket.io")(http,{
-    cors: {
-        origin: "*"
-    },
-    pingTimeout: 60000
-})
+const { Server } = require("ws")
+const { v4 } = require("uuid")
 
 console.log(`웹소켓 서버 실행 완료!`)
 
 let clients = {}
 let idlist = {}
 
-io.on("connection",(socket) => {
-    console.log(`새로운 클라이언트: ${socket.id}`)
+const server = new Server({port:80})
 
-    socket.on("join",(message) => {
-        console.log(`새로운 플레이어: ${message.name}(${message.id})`)
-        clients[message.id] = {name:message.name,color:message.color}
-        idlist[socket.id] = message.id
-        io.emit("join",clients)
+function broadcastall(message) {
+    server.clients.forEach(client => {
+        client.send(message)
+    })
+}
+
+function broadcast(message,ws) {
+    server.clients.forEach(client => {
+        if (client !== ws) client.send(message)
+    })
+}
+
+server.on("connection",(ws, req) => {
+    const ip = req.headers['x-forwarded-for'] | req.socket.remoteAddress
+    const socketid = v4()
+    console.log(`새로운 클라이언트: ${ip}(id: ${socketid})`)
+
+    ws.on("message",(message) => {
+        const data = JSON.parse(message.toString("utf-8"))
+        if (data.type == "join") {
+            console.log(`입장한 플레이어: ${data.name}(${data.id}`)
+            idlist[socketid] = {client:ws,id:data.id}
+            clients[data.id] = {color:data.color,name:data.name}
+            broadcastall(JSON.stringify({type:"join",clients}))
+        }
+        else if (data.type == "leave") {
+            console.log(`퇴장한 플레이어: ${data.name}(${data.id}`)
+            delete idlist[socketid]
+            delete clients[data.id]
+            broadcastall(JSON.stringify({type:"leave",clients}))
+        }
+        else if (data.type == "changecolor") {
+            clients[data.id].color = data.color
+            broadcastall(JSON.stringify(data))
+        }
+        else if (data.type == "changename") {
+            clients[data.id].name = data.name
+            broadcastall(JSON.stringify(data))
+        }
+        else if (data.type == "down") {
+            broadcast(JSON.stringify(data),true,ws)
+        }
+        else if (data.type == "up") {
+            broadcast(JSON.stringify(data),true,ws)
+        }
     })
 
-    socket.on("leave",(message) => {
-        console.log(`퇴장한 플레이어: ${message.name}(${message.id})`)
-        delete clients[message.id]
-        delete idlist[socket.id]
-        io.emit("leave",clients)
-    })
-
-    socket.on("changecolor",(message) => {
-        clients[message.id].color = message.color
-        io.emit("changecolor",message)
-    })
-
-    socket.on("changename",(message) => {
-        clients[message.id].name = message.name
-        io.emit("changename",message)
-    })
-
-    socket.on("down",(message) => {
-        socket.broadcast.emit("down",message)
-    })
-
-    socket.on("up",(message) => {
-        socket.broadcast.emit("up",message)
-    })
-
-    socket.on("disconnect",() => {
-        console.log(`연결을 끊은 클라이언트: ${socket.id}(${idlist[socket.id]})`)
-        delete clients[idlist[socket.id]]
-        delete idlist[socket.id]
-        io.emit("leave",clients)
+    ws.on("close",() => {
+        console.log(`연결을 끊은 클라이언트: ${ip}(id: ${socketid})`)
+        delete clients[idlist[socketid].id]
+        delete idlist[socketid]
+        broadcast(JSON.stringify({type:"leave",clients}))
     })
 })
 
-io.listen(80)
+//     socket.on("down",(message) => {
+//         socket.broadcast.emit("down",message)
+//     })
+
+//     socket.on("up",(message) => {
+//         socket.broadcast.emit("up",message)
+//     })
